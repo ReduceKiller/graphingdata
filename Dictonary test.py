3b4354ed59d89610fb0ba848ae63d8c119aec113
ReduceKiller <pha0015@jmss.vic.edu.au>
2015-09-25 21:34:06 +1000

    Version 2
    Did some dictionary tests

---
+++ Dictonary test.py
@@ -0,0 +1,6 @@
+A = {'Flying': 2, 'Feed': 1, 'Feed': 2, 'Resting': 1, 'Resting': 3, 'Feed': 1, 'Feed': 1, 'Feed': 3, 'Resting': 20, 'Resting': 6, 'Flying': 2,  'Flying': 1, 'Resting': 5}
+
+
+
+for s in A:
+    print(s)
