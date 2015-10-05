95d390ca93b48e78d8756d49678ebcbd6ba692fb
ReduceKiller <pha0015@jmss.vic.edu.au>
2015-10-05 11:44:14 +1100

    Version 6, multiple windows for graphs

--- Test run.py
+++ Test run.py
@@ -3,21 +3,36 @@ import random
 
 D = {'Feed': 1,  'Resting': 1, 'Flying': 2}
 
-title2 = input("Title of the graph?: ")
+C = {'Feed': 1,  'Resting': 1, 'Flying': 2}
 
-
+#title2 = input("Title of the graph?: ")
+#xlabel = input("")
+plt.figure()
+bar_graph = plt.bar(range(len(D)), D.values(), align='center')
+plt.xticks(range(len(D)), list(D.keys()))
+plt.figure()
 bar_graph = plt.bar(range(len(D)), D.values(), align='center')
 plt.xticks(range(len(D)), list(D.keys()))
 
-plt.title(title2)
-plt.ylabel("Frequency")
-plt.xlabel("Behaviours")
 
+plt.show()
+
+'''
+
+bar_graph2 = plt.bar(range(len(C)), C.values(), align='center')
+plt.figure()
+plt.xticks(range(len(C)), list(C.keys()))
 
 #c = random.choice(color)
 
 #bar_graph[0:range(len(D))].set_color(c)
-plt.show()
+
+'''
 
 
+'''
+plt.title(title2)
+plt.ylabel("Frequency")
+plt.xlabel("Behaviours")
+'''
 
